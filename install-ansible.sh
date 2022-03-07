6aaee665a3d3eefba4e491b9f92cba9eb3ac2bff
dyatesupnorth <dyates.upnorth@gmail.com>
2022-03-07 10:03:22 +0000

    moves scripts into one file

--- install-ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eEuo pipefail
-
-
-export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
-sudo pip3 install --upgrade pip
-pip3 install ansible
--- setup.sh
+++ setup.sh
@@ -1,7 +1,43 @@
 #!/bin/bash 
 set -eEuo pipefail
 
-./install-xcode.sh
-./generate-ssh-keys.sh
-./install-ansible.sh
-./pull-ansible-setup.sh
+if grep -q "version" <<< xcode-select --version; then
+    cat <<EOF >&2
+xcode seems to already be installed.
+EOF
+else
+    xcode-select --install 
+fi
+
+# ssh-keygen -t -f rsa ~/.ssh/id_rsa
+pbcopy < ~/.ssh/id_rsa.pub
+cat <<EOF >&2
+
+SSH Keys copied to clipboard, go to https://github.com/settings/keys
+and create a new key with a name. 
+EOF
+
+read -p "Press any key to continue ..."
+
+export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
+sudo pip3 install --upgrade pip
+pip3 install ansible
+
+BASE_DIRECTORY="$HOME/scripts"
+
+if [ ! -d "$BASE_DIRECTORY" ]; then
+  mkdir ~/scripts
+
+fi
+
+PLAYBOOK_DIR="$HOME/scripts/mac-dev-playbook"
+
+
+if [ ! -d "$PLAYBOOK_DIR" ] ; then
+    git clone git@github.com:dyatesupnorth/mac-dev-playbook.git "$PLAYBOOK_DIR"
+else
+    cd "$PLAYBOOK_DIR"
+    git pull git@github.com:dyatesupnorth/mac-dev-playbook.git
+    ansible-galaxy install -r requirements.yml
+    ansible-playbook main.yml --ask-become-pass
+fi
